8019dea89512765b4aa600761c8c2fda440d1089
Adam Grocholski <adgroc@microsoft.com>
2018-11-28 16:41:18 -0600

    update to build ps script

--- build.ps1
+++ build.ps1
@@ -16,5 +16,56 @@ foreach($parentDir in Get-ChildItem -Directory)
         {
             throw "Required file policy.config.json does not exist in $childDir."
         }
+
+        $configFile = "$parentDir\$childDir\policy.config.json"
+
+        $policyName  = (Get-Content $configFile | ConvertFrom-Json).config.policyName.value
+        
+        if(!$policyName)
+        {
+            throw "Required parameter policyName not specified in $configFile."
+        }
+
+        $policyDisplayName = (Get-Content $configFile | ConvertFrom-Json).config.policyDisplayName.value
+
+        if(!$policyDisplayName)
+        {
+            Write-Warning "Optional parameter policyDisplayName not specified in $configFile"
+        }
+
+        $policyDescription = (Get-Content $configFile | ConvertFrom-Json).config.policyDescription.value
+
+        if(!$policyDescription)
+        {
+            Write-Warning "Optional parameter policyDescription not specified in $configFile"
+        }
+
+        $assignmentName = (Get-Content $configFile | ConvertFrom-Json).config.assignmentName.value
+
+        if(!$assignmentName)
+        {
+            throw "Required parameter assignmentName not specified in $configFile."
+        }
+
+        $assignmentDisplayName = (Get-Content $configFile | ConvertFrom-Json).config.assignmentDisplayName.value
+
+        if(!$assignmentDisplayName)
+        {
+            Write-Warning "Required parameter assignmentDisplayName not specified in $configFile."
+        }
+
+        $assignmentDescription = (Get-Content $configFile | ConvertFrom-Json).config.assignmentDescription.value
+
+        if(!$assignmentDescription)
+        {
+            Write-Warning "Required parameter assignmentDescription not specified in $configFile."
+        }
+
+        $policySetName = (Get-Content $configFile | ConvertFrom-Json).config.policySetName.value
+
+        if(!$policySetName)
+        {
+            Write-Warning "Required parameter policySetName not specified in $configFile."
+        }
     }
 }
