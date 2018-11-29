50175a493757d5a057a7cdfaa5d89bbac12f9665
Adam Grocholski <adgroc@microsoft.com>
2018-11-29 13:43:48 -0600

    moar logging

--- release.ps1
+++ release.ps1
@@ -70,7 +70,7 @@ foreach($parentDir in Get-ChildItem -Directory)
             $cmdletParameters += @{Parameter=$paramsFile}
         }
 
-        Write-Warning "params: '$cmdletParameters'"
+        Write-Warning ($cmdletParameters | Out-String)
         &New-AzureRmPolicyDefinition @cmdletParameters
 
         if ($managementGroupName)
@@ -135,7 +135,7 @@ foreach($parentDir in Get-ChildItem -Directory)
             $cmdletParameters += @{PolicyParameter=$paramsFile}
         }
 
-        Write-Warning "params: '$cmdletParameters'"
+        Write-Warning ($cmdletParameters | Out-String)
         &New-AzureRmPolicyAssignment @cmdletParameters
     }
 }
