1fea50d187545ae764aebdf3b4b515665473ffc7
Adam Grocholski <adgroc@microsoft.com>
2018-11-28 18:01:33 -0600

    moved env variables to params

--- release.ps1
+++ release.ps1
@@ -1,40 +1,18 @@
-$resourceGroupName = "$(ResourceGroupName)"
-$subscriptionId = "$(SubscriptionId)"
-$managementGroupName = "$(ManagementGroupName)"
+param([string] $subscriptionId,
+      [string] $managementGroupName,
+      [string] $resourceGroupName)
 
 if(!$subscriptionId -and !$managementGroupName)
 {
-    Write-Error "Unable to create policy assignment: `$(SubscriptionId) '$subscriptionId' or `$(ManagementGroupName) '$managementGroupName' were not provided. Either may be provided, but not both."
+    Write-Error "Unable to create policy: `$(SubscriptionId) '$subscriptionId' or `$(ManagementGroupName) '$managementGroupName' were not provided. Either may be provided, but not both."
 }
 
 if ($subscriptionId -and $managementGroupName)
 {
-    Write-Error "Unable to create policy assignment: `$(SubscriptionId) '$subscriptionId' and `$(ManagementGroupName) '$managementGroupName' were both provided. Either may be provided, but not both."
+    Write-Error "Unable to create policy: `$(SubscriptionId) '$subscriptionId' and `$(ManagementGroupName) '$managementGroupName' were both provided. Either may be provided, but not both."
 }
 
 if ($managementGroupName -and $resourceGroupName)
 {
-    Write-Error "Unable to create policy assignment: `$(ManagementGroupName) '$managementGroupName' and `$(ResourceGroupName) '$resourceGroupName' were both provided. Either may be provided, but not both."
-}
-
-if ($managementGroupName)
-{
-    $scope = "/providers/Microsoft.Management/managementGroups/$managementGroupName"
-    $searchParameters = @{ManagementGroupName=$managementGroupName}
-}
-else
-{
-    if (!$subscriptionId)
-    {
-        $subscription = Get-AzureRmContext | Select-Object -Property Subscription
-        $subscriptionId = $subscription.Id
-    }
-
-    $scope = "/subscriptions/$subscriptionId"
-    $searchParameters = @{SubscriptionId=$subscriptionId}
-    
-    if ($resourceGroupName)
-    {
-        $scope += "/resourceGroups/$resourceGroupName"
-    }
+    Write-Error "Unable to create policy: `$(ManagementGroupName) '$managementGroupName' and `$(ResourceGroupName) '$resourceGroupName' were both provided. Either may be provided, but not both."
 }
